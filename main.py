63b1e6ec2b156c19ee7420ed0ee3162beccbbeb8
Sergej.Karaush <sergej.karaush@gmail.com>
2023-02-28 15:11:34 +0200

    add module_srrings

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import module_strings
+
 def get_number():
     import random
     return random.randint(1, 100)
 
 def get_hello():
     return 'Hello'
+
+if __name__ == '__maine__':
+    print(module_strings.func())
